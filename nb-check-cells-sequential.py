647ed4bee1fa1b21143abe2fe5f63398c60ee10a
Stef Smeets <s.smeets@esciencecenter.nl>
2021-12-06 14:48:09 +0100

    Implement check

--- nb-check-cells-sequential.py
+++ nb-check-cells-sequential.py
@@ -5,6 +5,21 @@ from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
+import nbformat
+
+
+def are_cells_sequential(filename):
+    node = nbformat.read(filename, as_version=4)
+    cells = node['cells']
+
+    for i, cell in enumerate(cells):
+        cell_number = int(cell['execution_count'])
+        expected_number = i + 1
+
+        if not cell_number == expected_number:
+            return False
+
+    return True
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -14,11 +29,11 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     retval = 0
     for filename in args.filenames:
-        print(filename)
-        with open(filename, 'rb') as f:
-            pass
+        if not are_cells_sequential(filename):
+            retval = 1
 
     return retval
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
