0b7b77059e0c803ff30535011399caf93972c3b4
Stef Smeets <s.smeets@esciencecenter.nl>
2021-12-06 14:47:00 +0100

    Add tests

---
+++ tests/test_nb_check_cells_sequential.py
@@ -0,0 +1,17 @@
+from nb_check_cells_sequential import are_cells_sequential
+import pytest
+from pathlib import Path
+
+TESTS_DIR = Path(__file__).parent
+
+
+@pytest.mark.parametrize("filename,expected_result", 
+(
+    (TESTS_DIR / 'dirty_order.ipynb', False),
+    (TESTS_DIR / 'clean_order.ipynb', True),
+))
+
+def test_are_cells_sequential(filename, expected_result):
+    result = are_cells_sequential(filename)
+
+    assert result == expected_result
